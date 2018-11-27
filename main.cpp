b50d3350dc7e0be30f5968cab917a19b67cd5d63
Anderson <andltayl@ut.utm.edu>
2018-11-27 12:45:07 -0600

    New main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+int sum(int num);
+
 int main(){
-  cout << "Hello World" << endl;
+  int num = 0;
+  
+  cout << "Please enter a number: " << endl;
+  cin >> num;
+
+  cout << sum(num) << endl;
+
+  
+  
   return 0;
+}
+
+int sum(int num){
+  int sum = 0;
+
+  for(int i = 0; i <= num; i++){
+    sum += i;
+  }
+  return sum;
 }
